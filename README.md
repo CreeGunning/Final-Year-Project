7ac4d152b2af2d2d167a4d956d651d6676c519e9
eg-creegunning <cree.gunning@eiregames.ie>
2023-05-13 02:13:13 +0100

    Final Project Complete

---
+++ README.md
@@ -0,0 +1,2 @@
+# Final Computing Project
+ 
---
+++ Source/kawatek_Project/kawatek_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "kawatek_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, kawatek_Project, "kawatek_Project" );
